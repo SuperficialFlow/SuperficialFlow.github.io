3342a8c2f6ba40f0468dd23f37f0818811b2ea63
SuperficialFlow <154480145+SuperficialFlow@users.noreply.github.com>
2024-01-16 00:37:52 +0800

    final

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function toggleMenu() {
 function scrollToContact() {
     document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');            
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
